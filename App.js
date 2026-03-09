e54a1212eae1d4b329562c38d6a35a8811f8889c
bgb051004-coder <bgb051004@gmail.com>
2026-03-09 08:15:31 +0700

    Bai 6.1

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
-  const [phoneNumber, setPhoneNumber] = React.useState('');
-  
-  const handleContinue = () => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [error, setError] = useState('');
+
+  const handlePhoneChange = (text) => {
+
+    // Chỉ giữ lại số
+    const cleaned = text.replace(/\D/g, '');
+
+    // Format dạng: 0912 345 678
+    const match = cleaned.match(/^(\d{0,4})(\d{0,3})(\d{0,3})$/);
+
+    if (match) {  
+      let formatted = match[1];
+      if (match[2]) formatted += ' ' + match[2];
+      if (match[3]) formatted += ' ' + match[3];
+
+      setPhoneNumber(formatted);
+    }
+
     const phoneRegex = /^0[0-9]{9}$/; // Kiểm tra định dạng số điện thoại
 
-    if(phoneNumber.trim() === '') {
-      alert('Vui lòng nhập số điện thoại');
-    } else if (!phoneRegex.test(phoneNumber)) {
-      alert('Số điện thoại không hợp lệ. Vui lòng nhập đúng định dạng.');
+    if(cleaned.trim() === '') {
+      setError('Vui lòng nhập số điện thoại');
+    } else if (!phoneRegex.test(cleaned)) {
+      setError('Số điện thoại không hợp lệ. Vui lòng nhập đúng định dạng.');
     } else {
-      // Xử lý tiếp tục đăng nhập hoặc đăng ký
-      alert('Số điện thoại hợp lệ: ' + phoneNumber);
+      setError('');
     }
   };
+
+  const handleContinue = () => {
+    const cleanPhone = phoneNumber.replace(/\s/g, '');
+    alert('Số điện thoại hợp lệ: ' + cleanPhone);
+  };
+
+  const isValid = error === '' && phoneNumber !== '';
   
   return (
     <View style={styles.container}>
@@ -35,11 +58,17 @@ export default function App() {
         style={styles.input}
         placeholder="Nhập số điện thoại của bạn"
         keyboardType="phone-pad"
+        value =  {phoneNumber}
+        onChangeText={handlePhoneChange}     
       />
-
-      
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Tiếp tục</Text>
+      {error != '' && <Text style={styles.errorText}>{error}</Text>}
+    
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: isValid ? '#007AFF' : '#e0e0e0' }]}
+        onPress={handleContinue}
+        disabled={!isValid}
+      >
+        <Text style={[styles.buttonText, { color: isValid ? '#fff' : '#999' }]}>Tiếp tục</Text>
       </TouchableOpacity>
 
       <StatusBar style="auto" />
@@ -92,5 +121,11 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 16,
     color: '#999',
+  },
+  errorText: {
+    color: 'red',
+    marginTop: 5,
+    marginBottom: 20,
+    fontSize: 14,
   },
 });
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Bạch Gia Bảo - 23810310340
+Bài 6.1: Ôn tập state - useState. TextInput - Validation và Format
+![App Screenshot](assets/1.jpg)
+![App Screenshot](assets/2.jpg)
+![App Screenshot](assets/3.jpg)
+![App Screenshot](assets/4.jpg)
